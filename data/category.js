1f2020d13e957aa9b190a7d628db4aec354e10a4
ANTHONY <anthonychukwudiDev@outlook.com>
2023-07-11 18:16:05 -0300

    adicionando imagens e alterando valores das categorias

---
+++ data/category.js
@@ -0,0 +1,275 @@
+const categorias = [
+  {
+    roupas: [
+      {
+        nome: "Calça",
+        imagem: "calcas.png",
+        tipos: [
+          {
+            nome: "Calça Jeans",
+            imagem: "",
+            peso: 500,
+          },
+          {
+            nome: "Calça Legging",
+            imagem: "",
+            peso: 300,
+          },
+          {
+            nome: "Calça Social",
+            imagem: "",
+            peso: 600,
+          },
+          {
+            nome: "Calça Cargo",
+            imagem: "",
+            peso: 700,
+          },
+          {
+            nome: "Calça de Moletom",
+            imagem: "",
+            peso: 400,
+          },
+        ],
+      },
+      {
+        nome: "Meias",
+        imagem: "meias.png",
+        tipos: [
+          {
+            nome: "Meias Curtas",
+            imagem: "",
+            peso: 50,
+          },
+          {
+            nome: "Meias Longas",
+            imagem: "",
+            peso: 80,
+          },
+          {
+            nome: "Meias Esportivas",
+            imagem: "",
+            peso: 70,
+          },
+          {
+            nome: "Meias de Algodão",
+            imagem: "",
+            peso: 60,
+          },
+          {
+            nome: "Meias de Lã",
+            imagem: "",
+            peso: 100,
+          },
+        ],
+      },
+      {
+        nome: "Blusa",
+        imagem: "moletom.png",
+        tipos: [
+          {
+            nome: "Blusa de Manga Comprida",
+            imagem: "",
+            peso: 250,
+          },
+          {
+            nome: "Blusa de Alça",
+            imagem: "",
+            peso: 150,
+          },
+          {
+            nome: "Blusa de Tricô",
+            imagem: "",
+            peso: 350,
+          },
+          {
+            nome: "Blusa de Moletom",
+            imagem: "",
+            peso: 400,
+          },
+          {
+            nome: "Blusa Peplum",
+            imagem: "",
+            peso: 200,
+          },
+        ],
+      },
+      {
+        nome: "Bermuda",
+        imagem: "shorts.png",
+        tipos: [
+          {
+            nome: "Bermuda Jeans",
+            imagem: "",
+            peso: 400,
+          },
+          {
+            nome: "Bermuda Cargo",
+            imagem: "",
+            peso: 300,
+          },
+          {
+            nome: "Bermuda de Praia",
+            imagem: "",
+            peso: 200,
+          },
+          {
+            nome: "Bermuda de Tecido",
+            imagem: "",
+            peso: 250,
+          },
+          {
+            nome: "Bermuda Ciclista",
+            imagem: "",
+            peso: 350,
+          },
+        ],
+      },
+      {
+        nome: "Tênis",
+        imagem: "tenis.png",
+        tipos: [
+          {
+            nome: "Tênis Esportivo",
+            imagem: "",
+            peso: 700,
+          },
+          {
+            nome: "Tênis Casual",
+            imagem: "",
+            peso: 600,
+          },
+          {
+            nome: "Tênis de Corrida",
+            imagem: "",
+            peso: 800,
+          },
+          {
+            nome: "Tênis de Skate",
+            imagem: "",
+            peso: 750,
+          },
+          {
+            nome: "Tênis de Couro",
+            imagem: "",
+            peso: 900,
+          },
+        ],
+      },
+      {
+        nome: "Roupas íntimas",
+        imagem: "roupas-intimas.png",
+        tipos: [
+          {
+            nome: "Sutiã",
+            imagem: "",
+            peso: 100,
+          },
+          {
+            nome: "Cueca Boxer",
+            imagem: "",
+            peso: 80,
+          },
+          {
+            nome: "Calcinha",
+            imagem: "",
+            peso: 50,
+          },
+          {
+            nome: "Cueca Slip",
+            imagem: "",
+            peso: 70,
+          },
+          {
+            nome: "Camisola",
+            imagem: "",
+            peso: 150,
+          },
+        ],
+      },
+      {
+        nome: "Boné",
+        imagem: "bone.png",
+        peso: 100,
+      },
+      {
+        nome: "Sandalias",
+        imagem: "sandalias.png",
+        tipos: [
+          {
+            nome: "Chinelo de Dedo",
+            imagem: "",
+            peso: 200,
+          },
+          {
+            nome: "Chinelo Slide",
+            imagem: "",
+            peso: 250,
+          },
+          {
+            nome: "Chinelo de Pano",
+            imagem: "",
+            peso: 150,
+          },
+          {
+            nome: "Chinelo de Borracha",
+            imagem: "",
+            peso: 180,
+          },
+          {
+            nome: "Chinelo de Couro",
+            imagem: "",
+            peso: 300,
+          },
+        ],
+      },
+      {
+        nome: "Touca",
+        imagem: "touca.png",
+        peso: 50,
+      },
+      {
+        nome: "Vestido",
+        imagem: "vestido.png",
+        peso: 400,
+      },
+      {
+        nome: "Saia",
+        imagem: "saia.png",
+        peso: 300,
+      },
+      {
+        nome: "Meia Calça",
+        imagem: "meias-calcas.png",
+        peso: 150,
+      },
+      {
+        nome: "Camiseta",
+        imagem: "camiseta.png",
+        peso: 200,
+      },
+      {
+        nome: "Sutiã",
+        imagem: "sutia.png",
+        peso: 100,
+      },
+      {
+        nome: "Body",
+        imagem: "body.png",
+        peso: 250,
+      },
+      {
+        nome: "Botas",
+        imagem: "botas.png",
+        peso: 900,
+      },
+      {
+        nome: "Cachecol",
+        imagem: "cachecol.png",
+        peso: 150,
+      },
+    ],
+  },
+];
+
+module.exports = categorias;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = 5000;
 const userRoutes = require("./routes/userRoutes");
 const authenticateToken = require("./middlewares/authMiddleware");
@@ -7,6 +8,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+app.use("/imagens", express.static(path.join(__dirname, "src", "assets")));//configuracao para servir imagens para o client
 app.use("/", userRoutes);
 app.use(authenticateToken); // Aplica o middleware de autenticação antes das rotas
 
